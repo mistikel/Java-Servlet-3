b1e866f11206b330e53d2cfa8234948df1079e8f
trainee <trainee>
2017-04-20 15:00:58 +0700

    exercise

---
+++ Java Servlet 3 Exercise/project2-exercise/SimpleApplication/src/com/mitrais/servlet/NameWrapper.java
@@ -0,0 +1,18 @@
+package com.mitrais.servlet;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+@SessionScoped
+@ManagedBean
+public class NameWrapper {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Java Servlet 3 Exercise/project3-exercise/SampleServlet/src/com/mitrais/servlet/Data.java
@@ -0,0 +1,22 @@
+package com.mitrais.servlet;
+
+public class Data {
+	private int id;
+	private String name;
+	public Data() {
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
